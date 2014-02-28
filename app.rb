913fe78535be51c82a95f3a520cdc875514431b2
Eric Kelly <heroiceric@gmail.com>
2014-02-28 08:45:49 -0500

    All the HTMLz

--- app.rb
+++ app.rb
@@ -1,5 +1,34 @@
 require 'sinatra'
 
+def posts
+  [
+    {
+      user: {
+        name: "Adam Sheehan",
+        avatar_url: "http://placekitten.com/g/50/50"
+      },
+      content: "I love kats but I can't spell.",
+      comments: [
+        {
+          user: {
+            name: "Helen Hood",
+            avatar_url: "http://placekitten.com/g/50/50"
+          },
+          content: "Cats are awesome! Especially Mr. G."
+        },
+        {
+          user: {
+            name: "Sam McTaggart",
+            avatar_url: "http://placekitten.com/g/50/50"
+          },
+          content: "I don't like cats."
+        }
+      ]
+    }
+  ]
+end
+
 get '/' do
+  @posts = posts
   erb :index
 end
